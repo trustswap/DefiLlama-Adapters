43fe6a932d0549d178ad21b5546e07fafd9f32d6
rohannero <rohanzarathustra@gmail.com>
2026-05-12 22:36:23 -0400

    refactor

--- projects/altera-finance/index.js
+++ projects/altera-finance/index.js
@@ -1,18 +1,18 @@
 const { sumTokens2 } = require('../helper/unwrapLPs')
+const { getLogs2 } = require('../helper/cache/getLogs')
 
-const POOLINFO = '0x744EaA81544E940F054AC1931025f1B6D26f2FDE'
-const PM = '0x1dc0d79dab5a4a630d2af3f9dc973af97f390427'
+const eventAbi = 'event StrategyDeployed(address indexed strategy, address indexed token0, address indexed token1, uint24 fee, int24 tickLower, int24 tickUpper)';
+const config = { ethereum: {factory: '0xf24F99795D1Cb1F0816101D4e0A41a84b44ac8c3', fromBlock: 24988527} }
 
 async function tvl(api) {
-  const pools = await api.call({
-    target: POOLINFO,
-    abi: 'function getAllPoolsTVL() view returns ((address token, uint8 decimals, uint256 amount)[])',
-  })
-  const tokens = pools.map(p => p.token)
-  return sumTokens2({ api, tokens, owner: PM, resolveUniV3: true })
+  const {factory, fromBlock} = config[api.chain]
+  const logs = await getLogs2({api, eventAbi, factory, fromBlock})
+  const strategies = logs.map(s => s.strategy)
+
+  return sumTokens2({ api, owners: strategies, resolveUniV3: true })
 }
 
 module.exports = {
-  start: 1776849595,
+  methodology: 'TVL is the value of all Uniswap v3 positions held by Altera strategy contracts.',
   ethereum: { tvl },
 }
